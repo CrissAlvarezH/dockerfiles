6c4054f8f59516769730bdee803de4641c30e1fe
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-06-25 17:57:51 -0500

    :sparkles: create command to upload static file to s3

--- automate-backups-to-s3/upload_static_files.sh
+++ automate-backups-to-s3/upload_static_files.sh
@@ -2,31 +2,30 @@
 
 set -e
 
+# TODO check if there are any folder to upload
+
 # set default values
 [ -z $STATIC_FILES_BACKUP_S3_FOLDER ] && STATIC_FILES_BACKUP_S3_FOLDER=static-files
 
 # validate env vars
 [ -z $S3_BUCKET -o -z $STATIC_FILES_BACKUP_S3_FOLDER ] \
     && echo "ERROR: S3 variables are required" && exit 2
-[ -z $STATIC_FILES_FOLDERS ] && echo "static files folders is not set, exit" && exit
 
 echo "upload static files"
 
+FOLDER_LOCATION="/backups/static-files"
 
-OIFS="$IFS"
-IFS=","
+for FOLDER in $(ls $FOLDER_LOCATION); do
+    echo "upload folder: $FOLDER"
 
-for FOLDER in $STATIC_FILES_FOLDERS; do
-    gzip -r "/backup/static-files/$FOLDER" > "/backip/static-files/$FOLDER.zip"
+    cd $FOLDER_LOCATION
+    zip -r "$FOLDER.zip" $FOLDER
 
     S3_STATIC_FILE_PATH="s3://${S3_BUCKET}/${STATIC_FILES_BACKUP_S3_FOLDER}/$FOLDER.zip"
 
-    aws s3 cp "/tmp/$FOLDER.zip" "$S3_STATIC_FILE_PATH"
+    aws s3 cp "$FOLDER.zip" "$S3_STATIC_FILE_PATH"
 
-    rm -rf "/tmp/$FOLDER.zip"
+    rm -rf "$FOLDER.zip"
 done
-
-IFS="$OIFS"
-unset OIFS
 
 echo "backup static file finish"
