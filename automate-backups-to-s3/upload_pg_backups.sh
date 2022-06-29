31700c97cb7d319b72c0c18dab6a85f007c38889
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-06-28 23:05:11 -0500

    :loud_sound: add date to logs

--- automate-backups-to-s3/upload_pg_backups.sh
+++ automate-backups-to-s3/upload_pg_backups.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-echo ""
-echo "========================================="
-echo "           PG BACKUP INIT"
-echo "-----------------------------------------"
-echo ""
+log() {
+    echo "$(date +"%Y-%m-%d %H:%M:%S"): $1"
+}
+
+log ""
+log "========================================="
+log "           PG BACKUP INIT"
+log "-----------------------------------------"
+log ""
 
 # set default variables
 [ -z $POSTGRES_PORT ] && POSTGRES_PORT=5432
@@ -15,11 +19,11 @@ echo ""
 
 # validate env vars
 [ -z $POSTGRES_HOST -o -z $POSTGRES_PORT -o -z $POSTGRES_USER -o -z $POSTGRES_PASSWORD ] \
-    && echo "ERROR: postgres credentials is required" && exit 2
+    && log " ERROR: postgres credentials is required" && exit 2
 [ -z $S3_BUCKET -o -z $PG_BACKUP_S3_FOLDER ] \
-    && echo "ERROR: S3 variables are required" && exit 2
+    && log " ERROR: S3 variables are required" && exit 2
 [ -z $DATABASE_NAMES -o -z $BACKUP_WITH_TIMESTAMP ] \
-    && echo "ERROR: DATABASE_NAMES and BACKUP_WITH_TIMESTAMP variables is required" && exit 2
+    && log " ERROR: DATABASE_NAMES and BACKUP_WITH_TIMESTAMP variables is required" && exit 2
 
 export PGPASSWORD=$POSTGRES_PASSWORD # export pass to pg_dump command use it
 
@@ -27,10 +31,10 @@ OIF="$IFS" # save original IFS (internal file separator) value to restore later
 IFS="," # set as , to split $DATABASE_NAMES
 
 for DATABASE in $DATABASE_NAMES; do
-    echo "- Create backup for '$DATABASE'"
+    log "- Create backup for '$DATABASE'"
     pg_dump -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $DATABASE | gzip > dump.sql.zip
 
-    echo "- Upload '$DATABASE' backup to s3"
+    log "- Upload '$DATABASE' backup to s3"
 
     if [ $BACKUP_WITH_TIMESTAMP == "true" ]; then
         S3_DESTINY="s3://${S3_BUCKET}/${PG_BACKUP_S3_FOLDER}/$DATABASE-$(date +"%Y-%m-%dT%H:%M:%SZ").sql.zip"
@@ -38,18 +42,20 @@ for DATABASE in $DATABASE_NAMES; do
         S3_DESTINY="s3://${S3_BUCKET}/${PG_BACKUP_S3_FOLDER}/$DATABASE.sql.zip"
     fi
 
-    aws s3 cp ./dump.sql.zip "$S3_DESTINY"
+    aws s3 cp --quiet ./dump.sql.zip "$S3_DESTINY"
+
+    log "- Uploaded file: $S3_DESTINY"
 
     rm -rf dump.sql.zip  # delete backup file
 
-    echo ""
+    log ""
 done
 
 # Restore origin IFS an unset temporal varaible
 IFS="$OIFS"
 unset OIFS
 
-echo "-----------------------------------------"
-echo "           PG BACKUP FINISH"
-echo "========================================="
-echo ""
+log "-----------------------------------------"
+log "           PG BACKUP FINISH"
+log "========================================="
+log ""
