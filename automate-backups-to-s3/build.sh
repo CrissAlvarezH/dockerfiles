f41fec1bd5532a95afa6bba5973fedf0d885d316
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-12-31 14:10:25 -0500

    :hammer: add --no-cache option to build command

--- automate-backups-to-s3/build.sh
+++ automate-backups-to-s3/build.sh
@@ -3,7 +3,7 @@
 version=$1
 [ -z $version ] && version=0.1 && echo "\nset defaul version 0.1\n"
 
-docker build -t crissalvarezh/automate-backups-to-s3:$version .
+docker build --no-cache -t crissalvarezh/automate-backups-to-s3:$version .
 
 docker tag crissalvarezh/automate-backups-to-s3:$version crissalvarezh/automate-backups-to-s3:latest
 
