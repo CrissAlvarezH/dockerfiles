bd68b9bfd424107240549ca9f1e18e9c38ed58cd
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2023-10-29 22:16:27 -0500

    :bug: add --platform=linux/amd64 on nginx-gunicorn-proxy dockerfile

--- nginx-gunicorn-proxy/build.sh
+++ nginx-gunicorn-proxy/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 version=$1
 [ -z $version ] && version=0.1 && echo "\nset defaul version 0.1\n"
@@ -6,6 +7,8 @@ version=$1
 docker build --no-cache -t crissalvarezh/nginx-gunicorn-proxy:$version .
 
 docker tag crissalvarezh/nginx-gunicorn-proxy:$version crissalvarezh/nginx-gunicorn-proxy:latest
+
+echo "\nPush image to DockerHub\n"
 
 docker push crissalvarezh/nginx-gunicorn-proxy:$version
 docker push crissalvarezh/nginx-gunicorn-proxy:latest
