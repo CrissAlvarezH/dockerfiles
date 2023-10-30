db2cbd704de29be73b20d4082dcf4e0b6e127fe2
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2023-10-30 01:07:23 -0500

    :wrench: delete LISTEN_PORT and set APP_HOST variable, and set 80 port as a unique port

--- nginx-gunicorn-proxy/run.sh
+++ nginx-gunicorn-proxy/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 # substitute env variables inside template and then put the output on nginx config files folder
-envsubst '$LISTEN_PORT:$APP_NAME:$APP_PORT:$STATIC_ROOT' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+envsubst '$APP_HOST' < /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 
 # daemon off if for running in foreground to see logs with docker
 nginx -g 'daemon off;'
